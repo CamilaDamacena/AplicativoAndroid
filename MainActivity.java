754d4902826942595fc2a33eb423abf035518f1a
CamilaDamacena <40028939+CamilaDamacena@users.noreply.github.com>
2018-06-12 13:56:39 -0300

    Atividade de Tópicos Especiais

---
+++ MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.meuapp.meuaplicativo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
